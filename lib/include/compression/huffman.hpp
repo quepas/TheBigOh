f09313d1fb8058d17cbe2468443fed2e3f7434f1
Quepas <oneinchman.dev@gmail.com>
2015-01-06 19:11:08 +0100

    Adding methods for finding char sequences, its unit tests. First draft for Hugmann algorithm.

--- lib/include/compression/huffman.hpp
+++ lib/include/compression/huffman.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
+#include "data/binary_tree.hpp"
+
 #include <map>
 #include <string>
 
 namespace oh {
   namespace compression {
+
+struct StringOccurrence
+{
+  StringOccurrence(std::string _str, int _occurrence)
+    : str(_str), occurrence(_occurrence) {}
+  std::string str;
+  int occurrence;
+};
 
 class StringComparer {
 public:
@@ -16,6 +26,10 @@ public:
 typedef std::map<std::string, int, StringComparer> StringOccurrences;
 
 StringOccurrences CountCharsOccurrence(std::string text, int sequence_length = 1);
+std::string FindMinOccurrence(StringOccurrences occurrences);
+std::pair<std::string, std::string> FindTwoMinOccurrence(StringOccurrences occurrences);
+
+data::BinaryTree<StringOccurrence> Huffman(StringOccurrences occurrences);
 
 }}
 // ~~ oh::compression
--- lib/src/compression/huffman.cpp
+++ lib/src/compression/huffman.cpp
@@ -2,8 +2,11 @@
 
 #include <algorithm>
 
+using oh::data::BinaryTree;
 using std::find;
+using std::pair;
 using std::map;
+using std::make_pair;
 using std::string;
 
 namespace oh {
@@ -21,6 +24,49 @@ StringOccurrences CountCharsOccurrence(string text, int sequence_length /*= 1*/)
     }
   }
   return result;
+}
+
+pair<string, string> FindTwoMinOccurrence(StringOccurrences occurrences)
+{
+  string first_occurrence = FindMinOccurrence(occurrences);
+  occurrences.erase(first_occurrence);
+  return make_pair(first_occurrence, FindMinOccurrence(occurrences));
+}
+
+string FindMinOccurrence(StringOccurrences occurrences)
+{
+  int min_occurrence = INT_MAX;
+  string min_str = "";
+  for (auto it = occurrences.begin(); it != occurrences.end(); ++it) {
+    if (it->second < min_occurrence) {
+      min_str = it->first;
+      min_occurrence = it->second;
+    }
+  }
+  return min_str;
+}
+
+data::BinaryTree<StringOccurrence> Huffman(StringOccurrences occurrences)
+{
+  if (occurrences.size() == 2) {
+    auto it = occurrences.begin();
+    auto first = StringOccurrence(it->first, it->second);
+    ++it;
+    auto second = StringOccurrence(it->first, it->second);
+    BinaryTree<StringOccurrence> tree(StringOccurrence("", first.occurrence + second.occurrence));
+    tree.AddLeftChild(first);
+    tree.AddRightChild(second);
+    return tree;
+  }
+
+  auto min_occurrences = FindTwoMinOccurrence(occurrences);
+  auto left = occurrences.find(min_occurrences.first);
+  auto right = occurrences.find(min_occurrences.second);
+
+  occurrences.erase(left->first);
+  occurrences.erase(right->first);
+  occurrences[left->first+right->first] = left->second + right->second;
+  auto tree = Huffman(occurrences);
 }
 
 }}
