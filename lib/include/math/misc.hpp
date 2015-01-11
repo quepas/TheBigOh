5d80d4ba5353347b0bf4d56c1f19fcaba13f4f06
Quepas <oneinchman.dev@gmail.com>
2015-01-11 02:01:26 +0100

    Adding method for testing floating-point value equality.

--- lib/include/math/misc.hpp
+++ lib/include/math/misc.hpp
@@ -1,15 +1,25 @@
 #pragma once
 
+#include <cmath>
+#include <limits>
+
 namespace oh {
   namespace math {
 
+const float PI_F = 3.14159265358979f;
+
+inline bool IsEqual(float x, float y)
+{
+  return std::abs(x - y) <= std::numeric_limits<float>::epsilon() * std::abs(x);
+}
+
 template<typename T>
-T min(T a, T b) {
+inline T min(T a, T b) {
   return a < b ? a : b;
 }
 
 template<typename T>
-T max(T a, T b) {
+inline T max(T a, T b) {
   return a > b ? a : b;
 }
 
